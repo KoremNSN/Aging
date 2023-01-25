23958575fe004a4fde647d06f5fde82929317e08
KoremNSN <40990514+KoremNSN@users.noreply.github.com>
2023-01-25 13:03:41 -0500

    Create update_q.py

---
+++ Reversal/update_q.py
@@ -0,0 +1,161 @@
+import theano.tensor as tt
+import pymc3 as pm
+
+def update_Q(stim, reward,
+             Qs,vec,
+             alpha, n_subj):
+    """
+    This function updates the Q table according to the RL update rule.
+    It will be called by theano.scan to do so recursevely, given the observed data and the alpha parameter
+    This could have been replaced be the following lamba expression in the theano.scan fn argument:
+        fn=lamba action, reward, Qs, alpha: tt.set_subtensor(Qs[action], Qs[action] + alpha * (reward - Qs[action]))
+    """
+     
+    PE = reward - Qs[tt.arange(n_subj), stim]
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], Qs[tt.arange(n_subj),stim] + alpha * PE)
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    return Qs, vec
+
+
+def update_Q_valence(stim, reward,
+             Qs,vec,
+             alpha_p, alpha_n, n_subj):
+    """
+    This function updates the Q table according to the RL update rule.
+    It will be called by theano.scan to do so recursevely, given the observed data and the alpha parameter
+    This could have been replaced be the following lamba expression in the theano.scan fn argument:
+        fn=lamba action, reward, Qs, alpha: tt.set_subtensor(Qs[action], Qs[action] + alpha * (reward - Qs[action]))
+    """
+     
+    PE = reward - Qs[tt.arange(n_subj), stim]
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], (tt.switch(tt.eq(reward,1),
+                                                                 Qs[tt.arange(n_subj),stim] + alpha_p * PE,
+                                                                 Qs[tt.arange(n_subj),stim] + alpha_n * PE)))
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    return Qs, vec
+
+def update_Q_half(stim, reward, trial,
+             Qs, vec, 
+             alpha_1, alpha_2,  n_subj):
+    """
+    This function updates the Q table according to the RL update rule.
+    It will be called by theano.scan to do so recursevely, given the observed data and the alpha parameter
+    This could have been replaced be the following lamba expression in the theano.scan fn argument:
+        fn=lamba action, reward, Qs, alpha: tt.set_subtensor(Qs[action], Qs[action] + alpha * (reward - Qs[action]))
+    """
+    PE = reward - Qs[tt.arange(n_subj), stim]
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], (tt.switch(tt.eq(trial,1),
+                                                                 Qs[tt.arange(n_subj),stim] + alpha_1 * PE,
+                                                                 Qs[tt.arange(n_subj),stim] + alpha_2 * PE)))
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    return Qs, vec
+ 
+def update_Q_valence_half(stim, reward, trial,
+             Qs, vec, 
+             alpha_p1, alpha_n1, alpha_p2, alpha_n2,  n_subj):
+    """
+    This function updates the Q table according to the RL update rule.
+    It will be called by theano.scan to do so recursevely, given the observed data and the alpha parameter
+    This could have been replaced be the following lamba expression in the theano.scan fn argument:
+        fn=lamba action, reward, Qs, alpha: tt.set_subtensor(Qs[action], Qs[action] + alpha * (reward - Qs[action]))
+    """
+    PE = reward - Qs[tt.arange(n_subj), stim]
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], (tt.switch(tt.eq(trial,1),
+                                                                 (tt.switch(tt.eq(reward,1),
+                                                                     Qs[tt.arange(n_subj),stim] + alpha_p1 * PE,
+                                                                     Qs[tt.arange(n_subj),stim] + alpha_n1 * PE)),
+                                                                 (tt.switch(tt.eq(reward,1),
+                                                                     Qs[tt.arange(n_subj),stim] + alpha_p2 * PE,
+                                                                     Qs[tt.arange(n_subj),stim] + alpha_n2 * PE)))))
+                                                                 
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    return Qs, vec
+
+def update_Q_decay(stim, reward,
+                   Qs,vec,
+                   alpha, decay, n_subj):
+    """
+    This function updates the Q table according to the RL update rule.
+    It will be called by theano.scan to do so recursevely, given the observed data and the alpha parameter
+    This could have been replaced be the following lamba expression in the theano.scan fn argument:
+        fn=lamba action, reward, Qs, alpha: tt.set_subtensor(Qs[action], Qs[action] + alpha * (reward - Qs[action]))
+    """
+     
+    PE = reward - Qs[tt.arange(n_subj), stim]
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], Qs[tt.arange(n_subj),stim]*decay + alpha * PE)
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    return Qs, vec
+
+
+def update_Q_hall(stim, reward,
+             Qs,vec,alpha,assoc,
+             eta,kappa, n_subj):
+    """
+    This function updates the Q table according to Hybrid PH model
+    For information, please see this paper: https://www.sciencedirect.com/science/article/pii/S0896627316305840?via%3Dihub
+  
+    """
+      
+    delta = reward - Qs[tt.arange(n_subj), stim]
+    alpha = tt.set_subtensor(alpha[tt.arange(n_subj), stim], eta * abs(delta) + (1-eta)*alpha[tt.arange(n_subj), stim])
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], Qs[tt.arange(n_subj),stim] + kappa*alpha[tt.arange(n_subj), stim] * delta)
+    
+    # in order to get a vector of expected outcome (dependent on the stimulus presentes [CS+, CS-] 
+    # we us if statement (switch in theano)
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+    
+    # we use the same idea to get the associability per trial
+    assoc = tt.set_subtensor(assoc[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                alpha[tt.arange(n_subj),1], alpha[tt.arange(n_subj),0])))
+    
+    return Qs, vec, alpha, assoc
+
+
+# Rouhani et al. version https://elifesciences.org/articles/61077
+# generate functions to run
+def update_Q_rounai(stim, reward,
+             Qs,vec,
+             eta,kappa,n_subj):
+    """
+    This function updates the Q table according to Hybrid PH model
+    For information, please see this paper: https://www.sciencedirect.com/science/article/pii/S0896627316305840?via%3Dihub
+  
+    """
+    
+    
+    delta = reward - Qs[tt.arange(n_subj), stim]
+    alpha = eta + (kappa * abs(delta))
+    alpha = 1 / (1 + pm.math.exp(-alpha)) # sigmoid function
+    Qs = tt.set_subtensor(Qs[tt.arange(n_subj),stim], Qs[tt.arange(n_subj),stim] + alpha * delta)
+    
+  
+    vec = tt.set_subtensor(vec[tt.arange(n_subj),0], (tt.switch(tt.eq(stim,1), 
+                                                                Qs[tt.arange(n_subj),1], Qs[tt.arange(n_subj),0])))
+        
+    return Qs, vec, alpha
